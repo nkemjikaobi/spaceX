026956670ffb9ceb219aedf2038807d69678635f
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2021-11-25 13:35:41 +0100

    Showed Launches & Launch Items

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,15 @@ import Launches from './components/Launches';
 import logo from './logo.png';
 
 const client = new ApolloClient({
-	uri: 'localhost://5000/graphql',
+	uri: 'http://localhost:5000/graphql',
 });
 
 const App = () => {
 	return (
 		<ApolloProvider client={client}>
 			<div className='container'>
-        <img src={logo} alt='SpaceX' />
-        <Launches />
+				<img src={logo} alt='SpaceX' />
+				<Launches />
 			</div>
 		</ApolloProvider>
 	);
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import LaunchItem from './LaunchItem';
 
 const LAUNCHES_QUERY = gql`
 	query LaunchesQuery {
 		launches {
 			flight_number
-			misson_name
+			mission_name
 			launch_date_local
 			launch_success
 		}
@@ -15,17 +16,22 @@ const LAUNCHES_QUERY = gql`
 
 const Launches = () => {
 	return (
-		<div>
+		<Fragment>
 			<h1 className='display-4 my-3'>Launches</h1>
 			<Query query={LAUNCHES_QUERY}>
 				{({ loading, error, data }) => {
 					if (loading) return <h4>Loading...</h4>;
 					if (error) console.log(error);
-					console.log({ data });
-					return <h1>Test</h1>;
+					return (
+						<Fragment>
+							{data.launches.map(launch => (
+								<LaunchItem key={launch.flight_number} launch={launch} />
+							))}
+						</Fragment>
+					);
 				}}
 			</Query>
-		</div>
+		</Fragment>
 	);
 };
 
