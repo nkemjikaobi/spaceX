739c4701a4cac8e766ed2aaf1d9b4f08c80bbf76
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2021-11-26 01:00:16 +0100

    Deployment

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Launches from './components/Launches';
 import logo from './logo.png';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:5000/graphql',
+	uri: '/graphql',
 });
 
 const App = () => {
